b0149db4a3485301c0c3c55fb98f525a7136f1a9
David Guzman <d.guzman@ucl.ac.uk>
2014-07-04 18:04:55 +0100

    [EIDPWebAppCache] : Implemented as a no-interface view session bean. Upgraded xml descriptors to EJB 3.1 spec

--- src/EIDPWebAppCache/com/eidp/webctrl/WebAppCache/EIDPWebAppCache.java
+++ src/EIDPWebAppCache/com/eidp/webctrl/WebAppCache/EIDPWebAppCache.java
@@ -5,7 +5,9 @@ import com.eidp.logger.Logger;
 import java.io.IOException ;
 import java.util.HashMap;
 import java.util.Vector;
+import javax.annotation.PreDestroy;
 import javax.ejb.EJBException;
+import javax.ejb.PostActivate;
 import javax.ejb.PrePassivate;
 import javax.ejb.Stateful;
 import javax.servlet.http.HttpSessionEvent;
@@ -38,248 +40,141 @@ public class EIDPWebAppCache implements HttpSessionListener {
     
     private javax.ejb.SessionContext context;
     private transient Logger logger;
-    private int logLevel = 9 ;
     private HashMap sessionData = new HashMap() ;
     private Vector userRoles = new Vector() ;
     private HashMap centerRoles = new HashMap() ;
-    private HashMap sessionRefs = new HashMap() ;
+    private final HashMap sessionRefs = new HashMap() ;
     private String sidePanelEntry = "" ;
     private String applicationContext = "" ;
+
+    public EIDPWebAppCache() {
+    }
     
-    /**
-     *
-     * @param sessionKey
-     * @param object
-     * @throws RemoteException
-     */
-    public void sessionRef_set( String sessionKey , Object object ) throws java.rmi.RemoteException {
+    public EIDPWebAppCache(String applicationname) {
+        this.applicationContext = applicationname ;
+        try {
+            this.logger = new Logger( "/com/eidp/" + applicationname + "/WebAppCache.log" ) ;
+            this.logger.logMessage( "Initializing WebAppCacheLog with Context: " + applicationname + "." ) ;
+        } catch (IOException e) {
+            throw new EJBException("[EIDPWebAppCache] : Cannot create instance of EIDPWebAppCache");
+        }
+    }
+    
+    public void sessionRef_set( String sessionKey , Object object ) {
         this.sessionRefs.put( sessionKey , object ) ;
     }
-    /**
-     *
-     * @param sessionKey
-     * @throws RemoteException
-     * @return
-     */
-    public Object sessionRef_get( String sessionKey ) throws java.rmi.RemoteException {
+
+    public Object sessionRef_get( String sessionKey ) {
         return this.sessionRefs.get( sessionKey ) ;
     }
-    /**
-     *
-     * @param sessionKey
-     * @throws RemoteException
-     */
-    public void sessionRef_remove( String sessionKey ) throws java.rmi.RemoteException {
+    
+    public void sessionRef_remove( String sessionKey ) {
         this.sessionRefs.remove( sessionKey ) ;
     }
     
-    /**
-     *
-     * @throws RemoteException
-     */
-    public void sessionRef_clear() throws java.rmi.RemoteException {
+    public void sessionRef_clear() {
         this.sessionRefs.clear() ;
     }
     
-    /**
-     *
-     * @param sessionDataKey
-     * @param object
-     * @throws RemoteException
-     */
-    public void sessionData_set( String sessionDataKey , Object object ) throws java.rmi.RemoteException {
+    public void sessionData_set( String sessionDataKey , Object object ) {
         this.sessionData.put( sessionDataKey , object ) ;
     }
-    /**
-     *
-     * @param sessionDataAll
-     * @throws RemoteException
-     */
-    public void sessionData_setAll( HashMap sessionDataAll ) throws java.rmi.RemoteException {
+    
+    public void sessionData_setAll( HashMap sessionDataAll ) {
         this.sessionData = sessionDataAll ;
     }
-    /**
-     *
-     * @param sessionDataKey
-     * @throws RemoteException
-     * @return
-     */
-    public Object sessionData_get( String sessionDataKey ) throws java.rmi.RemoteException {
+    
+    public Object sessionData_get( String sessionDataKey ) {
         return this.sessionData.get( sessionDataKey ) ;
     }
-    /**
-     *
-     * @param sessionDataKey
-     * @throws RemoteException
-     */
-    public void sessionData_remove( String sessionDataKey ) throws java.rmi.RemoteException {
+    
+    public void sessionData_remove( String sessionDataKey ) {
         this.sessionData.remove( sessionDataKey ) ;
     }
-    /**
-     *
-     * @param sessionDataKey
-     * @throws RemoteException
-     * @return
-     */
-    public boolean sessionData_containsKey( String sessionDataKey ) throws java.rmi.RemoteException {
+    
+    public boolean sessionData_containsKey( String sessionDataKey ) {
         return this.sessionData.containsKey( sessionDataKey ) ;
     }
-    /**
-     *
-     * @throws RemoteException
-     * @return
-     */
-    public boolean sessionData_exists() throws java.rmi.RemoteException {
+    
+    public boolean sessionData_exists() {
         if ( this.sessionData.isEmpty() || this.sessionData != null ) {
             return false ;
         } else return true ;
     }
-    /**
-     *
-     * @throws RemoteException
-     */
-    public void sessionData_clear() throws java.rmi.RemoteException {
+    
+    public void sessionData_clear() {
         this.sessionData.clear() ;
     }
     
-    /**
-     *
-     * @param object
-     * @throws RemoteException
-     */
-    public void userRoles_set( Object object ) throws java.rmi.RemoteException {
+    public void userRoles_set( Object object ) {
         this.userRoles.addElement( object ) ;
     }
-    /**
-     *
-     * @param index
-     * @throws RemoteException
-     * @return
-     */
-    public Object userRoles_get( int index ) throws java.rmi.RemoteException {
+
+    public Object userRoles_get( int index ) {
         return this.userRoles.get( index ) ;
     }
-    /**
-     *
-     * @throws RemoteException
-     * @return
-     */
-    public int userRoles_size() throws java.rmi.RemoteException {
+
+    public int userRoles_size() {
         return this.userRoles.size() ;
     }
-    /**
-     *
-     * @throws RemoteException
-     * @return
-     */
-    public boolean userRoles_exists() throws java.rmi.RemoteException {
+    
+    public boolean userRoles_exists() {
         if ( this.userRoles.size() > 0 ) {
             return true ;
         } else return false ;
     }
-    /**
-     *
-     * @param object
-     * @return
-     */
+
     public boolean userRoles_contains( Object object ) {
         return this.userRoles.contains( object ) ;
     }
+    
     public void userRoles_clear() {
         this.userRoles.clear() ;
     }
     
-    /**
-     *
-     * @param centerRolesKey
-     * @param object
-     * @throws RemoteException
-     */
-    public void centerRoles_set( String centerRolesKey , Object object ) throws java.rmi.RemoteException {
+    public void centerRoles_set( String centerRolesKey , Object object ) {
         this.centerRoles.put( centerRolesKey , object ) ;
     }
-    /**
-     *
-     * @param centerRolesAll
-     * @throws RemoteException
-     */
-    public void centerRoles_setAll( HashMap centerRolesAll ) throws java.rmi.RemoteException {
+
+    public void centerRoles_setAll( HashMap centerRolesAll ) {
         this.centerRoles = centerRolesAll ;
     }
-    /**
-     *
-     * @param centerRolesKey
-     * @throws RemoteException
-     * @return
-     */
-    public Object centerRoles_get( String centerRolesKey ) throws java.rmi.RemoteException {
+
+    public Object centerRoles_get( String centerRolesKey ) {
         return this.centerRoles.get( centerRolesKey ) ;
     }
-    /**
-     *
-     * @param centerRolesKey
-     * @throws RemoteException
-     */
-    public void centerRoles_remove( String centerRolesKey ) throws java.rmi.RemoteException {
+
+    public void centerRoles_remove( String centerRolesKey ) {
         this.centerRoles.remove( centerRolesKey ) ;
     }
-    /**
-     *
-     * @param centerRolesKey
-     * @throws RemoteException
-     * @return
-     */
-    public boolean centerRoles_containsKey( String centerRolesKey ) throws java.rmi.RemoteException {
+
+    public boolean centerRoles_containsKey( String centerRolesKey ) {
         return this.centerRoles.containsKey( centerRolesKey ) ;
     }
-    /**
-     *
-     * @throws RemoteException
-     * @return
-     */
-    public boolean centerRoles_exists() throws java.rmi.RemoteException {
+
+    public boolean centerRoles_exists() {
         if ( this.centerRoles.isEmpty() || this.centerRoles != null ) {
             return false ;
         } else return true ;
     }
-    /**
-     *
-     * @throws RemoteException
-     */
-    public void centerRoles_clear() throws java.rmi.RemoteException {
+
+    public void centerRoles_clear() {
         this.centerRoles.clear() ;
     }
-    /**
-     *
-     * @throws RemoteException
-     * @return
-     */
-    public HashMap centerRoles_getAll() throws java.rmi.RemoteException {
+
+    public HashMap centerRoles_getAll() {
         return this.centerRoles ;
     }
     
-    /**
-     *
-     * @param spe
-     * @throws RemoteException
-     */
-    public void sidePanelEntry_set( String spe ) throws java.rmi.RemoteException {
+    public void sidePanelEntry_set( String spe ) {
         this.sidePanelEntry = spe ;
     }
-    /**
-     *
-     * @throws RemoteException
-     * @return
-     */
-    public String sidePanelEntry_get() throws java.rmi.RemoteException {
+
+    public String sidePanelEntry_get() {
         return this.sidePanelEntry ;
     }
-    /**
-     *
-     * @throws RemoteException
-     * @return
-     */
-    public boolean sidePanelEntry_exists() throws java.rmi.RemoteException {
+
+    public boolean sidePanelEntry_exists() {
         if ( this.sidePanelEntry.equals( "" ) || this.sidePanelEntry == null ) {
             return false ;
         } else return true ;
@@ -292,52 +187,40 @@ public class EIDPWebAppCache implements HttpSessionListener {
         context=aContext;
     }
     
-    
-    /**
-     * @see javax.ejb.SessionBean#ejbActivate()
-     */
-    public void ejbActivate() {
+    @PostActivate
+    public void activate() {
         try {
             this.logger = new Logger( "/com/eidp/" + this.getApplicationContext() + "/WebAppCache.log" ) ;
-        } catch ( java.io.IOException ioe ) {
+        } catch ( java.io.IOException e ) {
+            throw new EJBException("[EIDPWebAppCache] : Cannot set applicationContext");
         }
     }
     
-    /**
-     * @see javax.ejb.SessionBean#ejbRemove()
-     */
-    public void ejbRemove() {
+    @PreDestroy
+    public void remove() {
     }
     
-    /**
-     *
-     * @param httpSessionEvent
-     */
+    @Override
     public void sessionCreated( HttpSessionEvent httpSessionEvent ) {
     }
     
-    /**
-     *
-     * @param httpSessionEvent
-     */
+    @Override
     public void sessionDestroyed( HttpSessionEvent httpSessionEvent ) {
-        this.ejbRemove() ;
+        this.remove() ;
     }
 
-    /**
-     * @return the applicationContext
-     */
     public String getApplicationContext() {
         return applicationContext;
     }
 
-    /**
-     * @param applicationContext the applicationContext to set
-     */
-    public void setApplicationContext(String applicationContext) throws IOException {
+    public void setApplicationContext(String applicationContext) {
         this.applicationContext = applicationContext;
-        this.logger = new Logger( "/com/eidp/" + applicationContext + "/WebAppCache.log" ) ;
-        this.logger.logMessage( "Initializing WebAppCacheLog with Context: " + applicationContext + "." ) ;
+        try {
+            this.logger = new Logger( "/com/eidp/" + applicationContext + "/WebAppCache.log" ) ;
+            this.logger.logMessage( "Initializing WebAppCacheLog with Context: " + applicationContext + "." ) ;
+        } catch (IOException e) {
+            throw new EJBException("[EIDPWebAppCache] : Cannot set applicationContext");
+        }
     }
     
     @PrePassivate
@@ -345,7 +228,7 @@ public class EIDPWebAppCache implements HttpSessionListener {
         try {
             this.logger.close();
         } catch (IOException ex) {
-            throw new EJBException("[EIDPWebAppCache] : @PrePassivate error; cannot close logger");
+            this.logger = null;
         }
         this.logger = null;
     }
